6587e9715513eaa6051b53a9ca172c85fdbb06c9
Magellnea <magellanea@gmail.com>
2015-07-07 00:23:56 +0200

    Minor formatting commit

--- src/main/java/graph/ArticulationPoints.java
+++ src/main/java/graph/ArticulationPoints.java
@@ -7,37 +7,40 @@ import java.util.List;
  */
 public class ArticulationPoints {
 
-        public static int dfsDepth;
-        public static List<Integer>[] adjList;
-        public static int[] dfsNum;
-        public static int[] dfsLow;
-        public static int[] dfsParent;
-        public static int UNVISITED = -1;
-        public static int rootChildren = 0;
-        public static int rootNode;
-        public static boolean[] articulateNodes;
+    public static int dfsDepth;
+    public static List<Integer>[] adjList;
+    public static int[] dfsNum;
+    public static int[] dfsLow;
+    public static int[] dfsParent;
+    public static int UNVISITED = -1;
+    // number of children for a given root
+    public static int rootChildren = 0;
+    // The current root node, added for when the root maybe an articulation point
+    public static int rootNode;
+    public static boolean[] articulateNodes;
 
     /**
      * Evaluate articulation points using Tarjan Algorithm.
      * To run the code check this method over all unvisited nodes, add
      * edge twice for undirected graphs
+     *
      * @param node The given node
      */
-        public static void findArticulatePoints(int node) {
-            dfsNum[node] = dfsLow[node] = dfsDepth++;
-            for (int element : adjList[node]) {
-                if (dfsNum[element] == UNVISITED) {
-                    if (node == rootNode) rootChildren++;
-                    dfsParent[element] = node;
-                    findArticulatePoints(element);
-                    if ((dfsParent[node]!=0) && (dfsLow[element] >= dfsNum[node]))
-                        //the node doesn't back-connect to an ancestor
-                        articulateNodes[node] = true;
-                    dfsLow[node] = Math.min(dfsLow[node], dfsLow[element]);
+    public static void findArticulatePoints(int node) {
+        dfsNum[node] = dfsLow[node] = dfsDepth++;
+        for (int element : adjList[node]) {
+            if (dfsNum[element] == UNVISITED) {
+                if (node == rootNode) rootChildren++;
+                dfsParent[element] = node;
+                findArticulatePoints(element);
+                if ((dfsParent[node] != 0) && (dfsLow[element] >= dfsNum[node]))
+                    //the node doesn't back-connect to an ancestor
+                    articulateNodes[node] = true;
+                dfsLow[node] = Math.min(dfsLow[node], dfsLow[element]);
 
-                } else if (element != dfsParent[node])
-                    dfsLow[node] = Math.min(dfsLow[node], dfsNum[element]);
-            }
+            } else if (element != dfsParent[node])
+                dfsLow[node] = Math.min(dfsLow[node], dfsNum[element]);
         }
+    }
 
 }
